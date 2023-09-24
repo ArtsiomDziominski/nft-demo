2ad851bb9ca647faeaf60689c5c85dd05050b629
Artem Deminski <artem.d@konomic.com>
2023-09-24 23:50:46 +0400

    update link to buy

--- pages/buy.vue
+++ pages/buy.vue
@@ -9,7 +9,7 @@
         <!--            Buy 1 ETH-->
         <!--          </v-btn>-->
         <!--        </a>-->
-        <NuxtLink to="https://testnets.opensea.io/collection/testtrucknft-27" target="_blank">
+        <NuxtLink to="https://testnets.opensea.io/collection/testtrucknft-38" target="_blank">
           <v-btn class="card__button" color="var(--main-green)">
             BUY
           </v-btn>
